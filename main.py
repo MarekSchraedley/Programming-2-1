27aeb12dc1eb9fa830c99e0837f19ed3808489eb
Marek Schraedley <112725894+MarekSchraedley@users.noreply.github.com>
2023-01-23 14:37:39 +0000

    update

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 print("hello world")
 x = input("whats your name?")
 print("hello " + x + "!")
+# this is a single-line comment
+""" arithmetic operators
++ add
+- subtract
+* Multiply
+/ Divide
+// Floor Divide (round down to nearest int)
+% Mod (Remainder from int division)
+** Pow
+"""
+
+""" Assignment Operators
+= Value assignment (x=y)
++= Add to (x += y is shorthand of x = x + y)
+-= subtract from ( x -= y is shorthand for  x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
+
+""" Data Types
+int Integer (Whole number)
+float - Floating-point number (number w/ a decimal)
+str = String(any text in quotes)
+bool Boolean (Either true or false)
+chr - Character (single keyboard character, in single-quotes)
+"""
---
+++ prog52a.py
@@ -0,0 +1,7 @@
+#lang52a - Area and perimiter of rectangle
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+area = length * width
+perim = 2 * length + 2 * width
+print("Area = " + str(area))
+print("Perimiter = " + str(perim))
